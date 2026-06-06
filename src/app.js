990b90178e470136876c975eff871a0b70ed302a
joacocaturelli <joacocatu@gmail.com>
2026-06-06 13:10:07 +0100

    Instaladas y añadidas dependencias cors y rateLimit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 import indexRoutes from "./routes/index.routes.js";
 import productsRoutes from "./routes/products.routes.js";
@@ -11,6 +14,27 @@ import { logger } from "./middlewares/logger.middleware.js";
 
 const app = express();
 
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: "https://localhost:5500" || "http://127.0.0.1:5500",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-type"],
+  }),
+);
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 10,
+  message: {
+    ok: false,
+    error: "Demasiadas peticiones, intentalo en 1 minuto",
+  },
+});
+
+app.use(limiter);
 app.use(logger);
 app.use(express.json()); // Para leer los datos del body
 app.use(express.urlencoded({ extended: true })); // Para leer los datos del body en formato urlencode
