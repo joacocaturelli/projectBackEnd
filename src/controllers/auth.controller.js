853c9bed509c2be548c758a68a7f894414f6dbee
joacocaturelli <joacocatu@gmail.com>
2026-06-27 19:02:52 +0100

    Chore: Aplicadas mejoras de manejo de errores y correccion de bugs en las capas de auth

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,40 +1,34 @@
 import * as authService from "../services/auth.service.js";
 import prisma from "../config/prismaClient.js";
 import { cookieOptions } from "../utils/common.utils.js";
+import { Selector } from "../utils/errors.utils.js";
 
 export const register = async (req, res, next) => {
-  try {
-    const { email, password, role } = req.body;
-    const result = await authService.registerUser({ email, password, role });
+  const { email, password, role } = req.body;
 
-    if (!result.ok) return next(new Error("No se pudo registrar el usuario"));
+  const result = await authService.registerUser({ email, password, role });
 
-    return res.status(201).json({
-      ok: true,
-    });
-  } catch (error) {
-    next(error);
-  }
+  if (!result.ok) return next(Selector.BAD_ERROR);
+
+  return res.status(201).json({
+    ok: true,
+    data: result.data,
+  });
 };
 
 export const login = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const result = await authService.loginUser({ email, password });
-    if (!result.ok) return next(new Error("No se pudo iniciar sesión"));
+  const result = await authService.loginUser({ email, password });
 
-    res.cookie("token", result.content, cookieOptions);
+  if (!result.ok) return next(Selector.WRONG_CRED);
 
-    return res.status(200).json({
-      ok: true,
-    });
-  } catch (error) {
-    return res.status(401).json({
-      ok: false,
-      error: { message: "Invalid credentials" },
-    });
-  }
+  res.cookie("token", result.content, cookieOptions);
+
+  return res.json({
+    ok: true,
+    data: "Welcome",
+  });
 };
 
 export const logout = (req, res) => {
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -10,17 +10,18 @@ export const registerUser = async ({ password, ...user }) => {
     const hashedPassword = await bcrypt.hash(isString(password), 10);
 
     // Creamos el usuario en la db con su mail, role y contraseña hasheada
-    await prisma.user.create({ data: { ...user, password: hashedPassword } });
+    const result = await prisma.user.create({ data: { ...user, password: hashedPassword } });
+
+    if (!result) throw new Error("Prisma no pudo crear el usuario");
 
     return {
       ok: true,
       data: "User registered",
     };
   } catch (error) {
-    console.log("Error registrando usuario", error.message);
+    console.log("Error registering user", error.message);
     return {
       ok: false,
-      error: error.message,
     };
   }
 };
@@ -31,19 +32,29 @@ export const loginUser = async ({ email, password }) => {
       select: { id: true, password: true, role: true },
       where: { email }, // Buscamos el usuario por su email
     });
+
     if (!user) throw new Error("User not found");
 
     const isValid = await bcrypt.compare(isString(password), user.password); // Comparamos su contraseña
-    if (!isValid) throw new Error("El email o la contraseña no son válidos");
 
-    const token = jwt.sign({ id: user.id, email, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" }); // Si es valida generamos el token
+    if (!isValid) throw new Error("Incorrect email or password");
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+    ); // Si es valida generamos el token
 
     return {
       ok: true,
       content: token,
     };
   } catch (error) {
-    console.log("Error al hacer el login", error.message);
+    console.log("Error logging user", error.message);
     return {
       ok: false,
     };
