5520fa928685308105396002233f885603647189
joacocaturelli <joacocatu@gmail.com>
2026-06-27 19:02:13 +0100

    Chore: Aplicadas mejoras de manejo de errores y correccion de bugs en las capas de products

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,102 +1,112 @@
 import * as productsService from "../services/products.service.js"; // Importamos el objeto con las funciones de services
+import { Selector } from "../utils/errors.utils.js";
+import { needNumber } from "../utils/common.utils.js";
 
 export const getProducts = async (req, res, next) => {
   // Funcion para obtener todos los productos
-  try {
-    const products = await productsService.getAllProducts();
+  const result = await productsService.getAllProducts();
 
-    return res.status(200).json({
-      ok: true,
-      data: products,
-    });
-  } catch (error) {
-    next(error);
-  }
+  if (!result.ok) return next(Selector.NOT_FOUND);
+
+  return res.json({
+    ok: true,
+    data: result.content,
+  });
 };
 
 export const getProduct = async (req, res, next) => {
   // Funcion para obtener un solo producto pasando un Id
-  try {
-    const product = await productsService.getProductById(parseInt(req.params.id));
+  const id = parseInt(req.params.id);
 
-    return res.status(200).json({
-      ok: true,
-      data: product,
-    });
-  } catch (error) {
-    next(error);
-  }
+  const result = await productsService.getProductById(id);
+
+  if (!result.ok) return next(Selector.NOT_FOUND);
+
+  return res.json({
+    ok: true,
+    data: result.content,
+  });
 };
 
 export const createOneProduct = async (req, res, next) => {
   // Funcion para crear un producto nuevo
-  try {
-    const { name, description, price, stock, imageUrl } = req.body; // Obtenemos todos los elementos del body pasados por el usuario
 
-    const newProduct = await productsService.createProduct({
-      name,
-      description,
-      price,
-      stock,
-      imageUrl,
-    });
+  const { name, description, price, stock, imageUrl } = req.body;
+  // Obtenemos todos los elementos del body pasados por el usuario
 
-    return res.status(201).json({
-      ok: true,
-      data: newProduct,
-    });
-  } catch (error) {
-    next(error);
+  const priceResult = needNumber(price);
+  if (!priceResult.ok) return next(Selector.BAD_INPUT);
+
+  if (stock) {
+    const stockResult = needNumber(stock);
+    if (!stockResult.ok) return next(Selector.BAD_INPUT);
   }
+
+  const result = await productsService.createProduct({
+    name,
+    description,
+    price,
+    stock,
+    imageUrl,
+  });
+
+  if (!result.ok) return next(Selector.BAD_ERROR);
+
+  return res.status(201).json({
+    ok: true,
+    data: result.content,
+  });
 };
 
 export const updateOneProduct = async (req, res, next) => {
-  try {
-    // Funcion para actualizar un producto
-    const { name, description, price, stock, imageUrl } = req.body; // Obtenemos todos los elementos del body pasados por el usuario
+  // Funcion para actualizar un producto
 
-    const newProductUpdated = await productsService.updateProduct(parseInt(req.params.id), {
-      // Actualizamos el producto
-      name,
-      description,
-      price,
-      stock,
-      imageUrl,
-    });
+  const id = parseInt(req.params.id);
+  const { name, description, price, stock, imageUrl } = req.body;
+  // Obtenemos todos los elementos del body pasados por el usuario
 
-    return res.status(200).json({
-      ok: true,
-      data: newProductUpdated,
-    });
-  } catch (error) {
-    if (error.code === "P2025") {
-      return res.status(404).json({
-        ok: false,
-        error: { message: "Producto no encontrado para actualizar" },
-      });
-    }
-
-    next(error);
+  if (price) {
+    const priceResult = needNumber(price);
+    if (!priceResult.ok) return next(Selector.BAD_INPUT);
   }
+
+  if (stock) {
+    const stockResult = needNumber(stock);
+    if (!stockResult.ok) return next(Selector.BAD_INPUT);
+  }
+
+  const result = await productsService.updateProduct(id, {
+    // Actualizamos el producto
+    name,
+    description,
+    price,
+    stock,
+    imageUrl,
+  });
+
+  if (result.error) return next(Selector.NOT_FOUND);
+
+  if (!result.ok) return next(Selector.BAD_ERROR);
+
+  return res.json({
+    ok: true,
+    data: result.content,
+  });
 };
 
 export const deleteOneProduct = async (req, res, next) => {
   // Funcion para eliminar un producto del array
-  try {
-    const productToDelete = await productsService.deleteProduct(parseInt(req.params.id));
 
-    return res.status(200).json({
-      ok: true,
-      data: productToDelete,
-    });
-  } catch (error) {
-    if (error.code === "P2025") {
-      return res.status(404).json({
-        ok: false,
-        error: { message: "Producto no encontrado para eliminar" },
-      });
-    }
+  const id = parseInt(req.params.id);
 
-    next(error);
-  }
+  const result = await productsService.deleteProduct(id);
+
+  if (result.error) return next(Selector.NOT_FOUND);
+
+  if (!result.ok) return next(Selector.BAD_ERROR);
+
+  return res.json({
+    ok: true,
+    data: result.content,
+  });
 };
--- src/services/products.service.js
+++ src/services/products.service.js
@@ -2,41 +2,118 @@ import prisma from "../config/prismaClient.js";
 
 export const getAllProducts = async (productsIds) => {
   try {
+    const result = await prisma.product.findMany({
+      omit: { createdAt: true },
+      where: { id: { in: productsIds } },
+    });
+
+    if (!result) throw new Error("Productos no obtenidos desde prisma");
+
     return {
       ok: true,
-      content: await prisma.product.findMany({
-        omit: { createdAt: true },
-        where: { id: { in: productsIds } },
-      }),
+      content: result,
     };
   } catch (error) {
-    console.log("Error obteniendo todos los productos", error.message);
+    console.log("Error geting products", error.message);
     return {
       ok: false,
-      content: [],
     };
   }
 };
 
-export const getProductById = (id) => {
-  return prisma.product.findUnique({
-    where: { id },
-  });
+export const getProductById = async (id) => {
+  try {
+    const result = await prisma.product.findUnique({
+      where: { id },
+    });
+
+    if (!result) throw new Error("Producto no obtenido desde prisma");
+
+    return {
+      ok: true,
+      content: result,
+    };
+  } catch (error) {
+    console.log("Error geting product by Id", error.message);
+    return {
+      ok: false,
+    };
+  }
 };
 
-export const createProduct = (data) => {
-  return prisma.product.create({ data });
+export const createProduct = async (data) => {
+  try {
+    const result = await prisma.product.create({ data });
+
+    if (!result) throw new Error("Producto no creado desde prisma");
+
+    return {
+      ok: true,
+      content: result,
+    };
+  } catch (error) {
+    console.log("Error creating product", error.message);
+    return {
+      ok: false,
+    };
+  }
 };
 
-export const updateProduct = (id, data) => {
-  return prisma.product.update({
-    where: { id },
-    data,
-  });
+export const updateProduct = async (id, data) => {
+  try {
+    const result = await prisma.product.update({
+      where: { id },
+      data,
+    });
+
+    if (!result) throw new Error("Producto no actualizado desde prisma");
+
+    return {
+      ok: true,
+      content: result,
+    };
+  } catch (error) {
+    if (error.code === "P2025") {
+      console.log("Error updating product", error.message);
+
+      return {
+        ok: false,
+        error: "Product not found",
+      };
+    }
+
+    console.log("Error updating product", error.message);
+    return {
+      ok: false,
+    };
+  }
 };
 
-export const deleteProduct = (id) => {
-  return prisma.product.delete({
-    where: { id },
-  });
+export const deleteProduct = async (id) => {
+  try {
+    const result = await prisma.product.delete({
+      where: { id },
+    });
+
+    if (!result) throw new Error("Producto no eliminado desde prisma");
+
+    return {
+      ok: true,
+      content: result,
+    };
+  } catch (error) {
+    if (error.code === "P2025") {
+      console.log("Error deleting product", error.message);
+
+      return {
+        ok: false,
+        error: "Product not found",
+      };
+    }
+
+    console.log("Error deleting product", error.message);
+    return {
+      ok: false,
+    };
+  }
 };
