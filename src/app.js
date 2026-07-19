1a353bd1e8c14ccbffd7b3375dc1b9a3c87b5d33
joacocaturelli <joacocatu@gmail.com>
2026-07-19 17:52:38 +0100

    Fix: Corregido el puerto que permite cors() para usar la app en front

--- src/app.js
+++ src/app.js
@@ -24,7 +24,12 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    origin: [
+      "http://localhost:3000",
+      "http://127.0.0.1:3000",
+      "http://localhost:5173", //Frontend Vite
+      "http://127.0.0.1:5173",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-type"],
