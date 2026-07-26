d4907e2c12ba26eb226f774b9bb3b30d646dfd60
joacocaturelli <joacocatu@gmail.com>
2026-07-26 21:51:55 +0100

    Fix: corse ahora permite la cabecera Authorization

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.use(
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-type"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
