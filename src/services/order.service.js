5c41a6fdf8c774df74128378d1948c30a5aba1c7
joacocaturelli <joacocatu@gmail.com>
2026-08-13 10:32:53 +0100

    Chore: Ordenadas las funciones

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -1,5 +1,27 @@
 import prisma from "../config/prismaClient.js";
 
+export const getUserOrders = async (userId) => {
+  try {
+    const orders = await prisma.order.findMany({
+      where: { userId },
+      include: {
+        items: true,
+      },
+      orderBy: { createdAt: "desc" }, // Mas recientes primero
+    });
+
+    return {
+      ok: true,
+      content: orders,
+    };
+  } catch (error) {
+    console.log("Error getting user orders:", error.message);
+    return {
+      ok: false,
+    };
+  }
+};
+
 export const getOrderById = async (orderId, userId) => {
   try {
     const order = await prisma.order.findUnique({
@@ -19,28 +41,6 @@ export const getOrderById = async (orderId, userId) => {
     };
   } catch (error) {
     console.log("Error getting order:", error.message);
-    return {
-      ok: false,
-    };
-  }
-};
-
-export const getUserOrders = async (userId) => {
-  try {
-    const orders = await prisma.order.findMany({
-      where: { userId },
-      include: {
-        items: true,
-      },
-      orderBy: { createdAt: "desc" }, // Mas recientes primero
-    });
-
-    return {
-      ok: true,
-      content: orders,
-    };
-  } catch (error) {
-    console.log("Error getting user orders:", error.message);
     return {
       ok: false,
     };
