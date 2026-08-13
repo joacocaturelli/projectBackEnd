1760c6fcecaa35ae530f30b6c94cef0f76405614
joacocaturelli <joacocatu@gmail.com>
2026-08-13 10:31:57 +0100

    Chore: Ordenadas las funciones

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,11 +1,10 @@
 import * as orderService from "../services/order.service.js";
 import { Selector } from "../utils/errors.utils.js";
 
-export const getOrderById = async (req, res, next) => {
-  const { orderId } = req.params;
-  const { id: userId } = res.locals; // Usuario autenticado
+export const getUserOrders = async (req, res, next) => {
+  const { id: userId } = res.locals;
 
-  const result = await orderService.getOrderById(orderId, userId);
+  const result = await orderService.getUserOrders(userId);
 
   if (!result.ok) return next(Selector.NOT_FOUND);
 
@@ -15,10 +14,11 @@ export const getOrderById = async (req, res, next) => {
   });
 };
 
-export const getUserOrders = async (req, res, next) => {
-  const { id: userId } = res.locals;
+export const getOrderById = async (req, res, next) => {
+  const { orderId } = req.params;
+  const { id: userId } = res.locals; // Usuario autenticado
 
-  const result = await orderService.getUserOrders(userId);
+  const result = await orderService.getOrderById(orderId, userId);
 
   if (!result.ok) return next(Selector.NOT_FOUND);
 
